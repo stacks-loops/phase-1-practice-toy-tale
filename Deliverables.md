104400ac1f6366f85c8b0998c4657ec80fe047ee
Parker <p.cronin.connolly@gmail.com>
2024-01-17 19:27:52 -0500

    picture cards rendering

---
+++ Deliverables.md
@@ -0,0 +1,7 @@
+Start and json server
+Make a fetch request that renders the toys to the dom via a "card"
+Connect a form that will allow "Add Toy" card
+Toys persist
+Click event listener on a like button that we create
+Likes should go up when clicked
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-let addToy = false;
+document.addEventListener('DOMContentLoaded', () => {
+fetch('http://localhost:3000/toys')
+.then(resp => resp.json())
+.then(data => renderToys(data))
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
-});
+function renderToys(toysArray) {
+  const toysContainer = document.querySelector('#toy-collection')
+
+  toysArray.forEach((toyObj) => {
+    const eachCard = document.createElement('div')
+    eachCard.classList.add('card')
+
+    const name = document.createElement('h2')
+    const image = document.createElement('img')
+    const likes = document.createElement('h2')
+
+    name.innerText = toyObj.name
+    likes.innerText = toyObj.likes
+    image.src = toyObj.image
+
+    eachCard.append(name, image, likes)
+    toysContainer.append(eachCard)
+
+   
+  })
+  
+
+}
+})
