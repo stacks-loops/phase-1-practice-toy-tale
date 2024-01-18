21442679da70762a0b8d7bd2cf0777c31d933f48
Parker <p.cronin.connolly@gmail.com>
2024-01-17 19:45:56 -0500

    template code added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+let addToy = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector("#new-toy-btn");
+  const toyFormContainer = document.querySelector(".container");
+  addBtn.addEventListener("click", () => {
+    // hide & seek with the form
+    addToy = !addToy;
+    if (addToy) {
+      toyFormContainer.style.display = "block";
+    } else {
+      toyFormContainer.style.display = "none";
+    }
+  });
+});
+
 fetch('http://localhost:3000/toys')
 .then(resp => resp.json())
 .then(data => renderToys(data))
@@ -28,4 +43,4 @@ function renderToys(toysArray) {
    
   })
 }
-})
+
